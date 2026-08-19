9a785b15f0da85931310c5378154e221d763f63e
vishnu564494 <palnativishnu5645@gmail.com>
2026-08-19 22:12:03 +0530

    Add description for delivery robot navigation script
    Added a comment explaining the purpose of the script.

--- LAB EXPERIMENTS/EXP.7.py
+++ LAB EXPERIMENTS/EXP.7.py
@@ -1,3 +1,9 @@
+#A delivery robot operates in a warehouse with predefined delivery points. Using Bellman
+equations, compute the state-value function for navigating to each delivery point.
+Implement this in Python and visualize the value function for different policies.
+
+
+
 from google.colab import files
 import pandas as pd
 import numpy as np
